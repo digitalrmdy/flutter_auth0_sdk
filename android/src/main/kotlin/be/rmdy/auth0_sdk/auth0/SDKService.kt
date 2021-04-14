f2c78e981267acae7e9f6a43ee245989972e1907
Mathias Cochet <mathias.cochet@rmdy.be>
2021-04-14 11:21:31 +0200

    code format and clean up

--- android/src/main/kotlin/be/rmdy/auth0_sdk/auth0/SDKService.kt
+++ android/src/main/kotlin/be/rmdy/auth0_sdk/auth0/SDKService.kt
@@ -15,20 +15,21 @@ import com.auth0.android.result.DatabaseUser
 class SDKService {
 
     var initialized = false
-    var account:Auth0? = null
+    var account: Auth0? = null
     var apiClient: AuthenticationAPIClient? = null
 
     fun initialize(clientId: String, domain: String, onResult: (Boolean) -> Unit) {
-            this.account = Auth0(clientId, domain)
-            // Only enable network traffic logging on production environments!
-            //this.account!!.networkingClient = DefaultClient(enableLogging = true)
-            this.apiClient =  AuthenticationAPIClient(this.account!!)
-            initialized = true
-            onResult(true)
+        this.account = Auth0(clientId, domain)
+        // Only enable network traffic logging on production environments!
+        //this.account!!.networkingClient = DefaultClient(enableLogging = true)
+        this.apiClient = AuthenticationAPIClient(this.account!!)
+        initialized = true
+        onResult(true)
     }
 
     fun loginWithEmailAndPassword(email: String, password: String, onResult: (LoginResult) -> Unit) {
-        apiClient?.login(email, password)?.setScope("openid profile email offline_access")?.start(object : Callback<Credentials, AuthenticationException> {
+        apiClient?.login(email, password)?.setScope("openid profile email offline_access")?.start(object :
+                Callback<Credentials, AuthenticationException> {
             override fun onFailure(error: AuthenticationException) {
                 onResult(LoginError(code = error.statusCode, message = error.message
                         ?: "error while logging in with username/password "))
@@ -45,7 +46,8 @@ class SDKService {
         if (name.isNotBlank()) {
             metadata["name"] = name
         }
-        apiClient?.createUser(email = email, password = password, connection = connection)?.addParameters(metadata)?.start(object : Callback<DatabaseUser, AuthenticationException> {
+        apiClient?.createUser(email = email, password = password, connection = connection)?.addParameters(metadata)?.start(object :
+                Callback<DatabaseUser, AuthenticationException> {
             override fun onFailure(error: AuthenticationException) {
                 onResult(RegisterError(code = error.statusCode, message = error.message
                         ?: "error while registering with username/password "))
@@ -57,15 +59,15 @@ class SDKService {
         })
     }
 
-    fun authWithGoogle(context: Context, onResult: (LoginResult) -> Unit){
+    fun authWithGoogle(context: Context, onResult: (LoginResult) -> Unit) {
         authWithSocialProvider(connection = "google-oauth2", context = context, onResult = onResult)
     }
 
-    fun authWithApple(context: Context, onResult: (LoginResult) -> Unit){
+    fun authWithApple(context: Context, onResult: (LoginResult) -> Unit) {
         authWithSocialProvider(connection = "apple", context = context, onResult = onResult)
     }
 
-    private fun authWithSocialProvider(connection: String, context: Context, onResult: (LoginResult) -> Unit){
+    private fun authWithSocialProvider(connection: String, context: Context, onResult: (LoginResult) -> Unit) {
         WebAuthProvider.login(this.account!!)
                 .withScope("openid profile email offline_access")
                 .withConnection(connection)
@@ -82,9 +84,9 @@ class SDKService {
     }
 
     fun resetPassword(email: String, connection: String, onResult: (ResetPasswordResult) -> Unit) {
-        apiClient?.resetPassword(email= email, connection = connection)?.start(object : Callback<Void?, AuthenticationException> {
+        apiClient?.resetPassword(email = email, connection = connection)?.start(object : Callback<Void?, AuthenticationException> {
             override fun onFailure(error: AuthenticationException) {
-                onResult(ResetPasswordError(code= error.statusCode, message = error.message ?: "error while resetting password"))
+                onResult(ResetPasswordError(code = error.statusCode, message = error.message ?: "error while resetting password"))
             }
 
             override fun onSuccess(result: Void?) {
